5b59be12422e715f1789326010a0c5fbd90588cb
houlee <houliqiang@hotmail.com>
2019-12-18 11:50:48 +0800

    uwsgi设置

--- papi/papi/wsgi.py
+++ papi/papi/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/opt/server/python3.7/lib/python3.7/site-packages')
+sys.path.append('/opt/server/python3.7/lib/python3.7/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'papi.settings')
 
 application = get_wsgi_application()
